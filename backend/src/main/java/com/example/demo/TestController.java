425154d755dbf50e428e5fa7e62ea2111794db93
tnnhuaa <nguyenhientuananh@gmail.com>
2026-03-02 21:34:46 +0700

    feat: Add Test controller

--- backend/src/main/java/com/example/demo/TestController.java
+++ backend/src/main/java/com/example/demo/TestController.java
@@ -1,9 +1,17 @@
 package com.example.demo;
 
-/**
-*   Package: com.example.demo
-*   Created by @tnnhuaa
-*   Date: 02/03/2026 - 8:59 CH
-*   Description: ...
-*/public class TestController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public Map<String, String> pingPong() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Kết nối thành công! Lời chào từ Spring Boot.");
+        return response;
+    }
 }
