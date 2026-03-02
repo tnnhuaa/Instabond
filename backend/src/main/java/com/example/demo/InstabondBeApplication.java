99c27f6530f95b16de97bcf036be7cc2002e5065
tnnhuaa <nguyenhientuananh@gmail.com>
2026-03-02 18:10:35 +0700

    init: Set up frontend & backend

---
+++ backend/src/main/java/com/example/demo/InstabondBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstabondBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstabondBeApplication.class, args);
+	}
+
+}
