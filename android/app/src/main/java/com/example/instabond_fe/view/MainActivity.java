0a0295c879148e8aa96abe82de0c6b64ca472ef2
tnnhuaa <nguyenhientuananh@gmail.com>
2026-03-06 22:07:31 +0700

    feat: Design UI for Newsfeed #1

--- android/app/src/main/java/com/example/instabond_fe/view/MainActivity.java
+++ android/app/src/main/java/com/example/instabond_fe/view/MainActivity.java
@@ -1,55 +1,27 @@
 package com.example.instabond_fe.view;
 
 import android.os.Bundle;
-import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.instabond_fe.R;
-import com.example.instabond_fe.network.ApiService;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import com.example.instabond_fe.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // 1. Cấu hình Retrofit trỏ tới máy ảo
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:8080/") // IP ma thuật cho máy ảo
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        // Thiết lập Toolbar
+        setSupportActionBar(binding.toolbar);
 
-        // 2. Tạo service
-        ApiService apiService = retrofit.create(ApiService.class);
-
-        // 3. Gọi API Test
-        apiService.testConnection().enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if (response.isSuccessful()) {
-                    try {
-                        String result = response.body().string();
-                        Log.d("TEST_API", "Thành công: " + result);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    Log.e("TEST_API", "Lỗi Server: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Log.e("TEST_API", "Tạch kết nối: " + t.getMessage());
-            }
+        // Thiết lập Bottom Navigation
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            // Xử lý chuyển tab sau này
+            return true;
         });
     }
 }
