1068539dac99cf6bf30dec877eed583f7ae9befb
Kugoo <khoanqd0807@gmail.com>
2026-03-26 21:45:43 +0700

    feat(config): add mail starter, async support and mail configurations

--- backend/src/main/java/com/instabond/InstabondBeApplication.java
+++ backend/src/main/java/com/instabond/InstabondBeApplication.java
@@ -2,12 +2,12 @@ package com.instabond;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class InstabondBeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InstabondBeApplication.class, args);
 	}
-
 }
